6e45b7589aa6237da2c2cea9c6fff020ab5607c9
Konopleva_IV <inna.konopleva@evraz.com>
2025-04-12 22:13:25 +0300

    скрипт VHF NAVAID (D)

--- Scripts/string_to_table.js
+++ Scripts/string_to_table.js
@@ -4,25 +4,25 @@ function checkCharacters(input) {
 
     switch (input[0]) {
         case 'S': // Если первый символ 'S'
-            messages.push("S: Standard [1]")
+            messages.push("<b>S:</b> Standard [1]")
             if (input.length >= 5) { // Проверяем 5 символ
                 switch (input[4]) {
                 case 'A':
-                    messages.push("A: MORA [5]")
+                    messages.push("<b>A:</b> MORA [5]")
                     if (input.length >= 6) { // Проверяем 6 символ
                         switch (input[5]) {
                         case 'S':
-                            messages.push("S: Grid MORA (AS) [6]")
+                            messages.push('<b>S:</b> Grid MORA (AS) [6] (<a href="References/5_5_Subsection_Code.png" target="_blank">5.5</a>)')
                             if (input.length >= 16) {
-                                messages.push(input.substring(13,16) + ": Starting Latitude [14-16]")}
+                                messages.push('<b>' + input.substring(13,16) + ':</b> Starting Latitude [14-16] (<a href="References/5_141_Starting_Latitude.png" target="_blank">5.141</a>)')}
                             if (input.length >= 20) {
-                                messages.push(input.substring(16,20) + ": Starting Longitude [17-20]")}
+                                messages.push('<b>' + input.substring(16,20) + ':</b> Starting Longitude [17-20] (<a href="References/5_142_Starting_Longitude.png" target="_blank">5.142</a>)')}
                             if (input.length >= 33) {
-                                messages.push(input.substring(30,33) + ": MORA 1 [31-33]")}
+                                messages.push('<b>' + input.substring(30,33) + ':</b> MORA 1 [31-33] (<a href="References/5_143_MORA.png" target="_blank">5.143</a>)')}
                             if (input.length >= 36) {
-                                messages.push(input.substring(33,36) + ": MORA 2 [34-36]")}
+                                messages.push('<b>' + input.substring(33,36) + ':</b> MORA 2 [34-36] (<a href="References/5_143_MORA.png" target="_blank">5.143</a>)')}
                             if (input.length >= 39) {
-                                messages.push(input.substring(36,39) + ": MORA 3 [37-39]")}
+                                messages.push('<b>' + input.substring(36,39) + ':</b> MORA 3 [37-39] (<a href="References/5_143_MORA.png" target="_blank">5.143</a>)')}
                             break;
                         default:
                             messages.push("6 - ???")
@@ -31,11 +31,47 @@ function checkCharacters(input) {
                     }
                     break;
                 case 'D':
-                    messages.push("5 - D - NAVAID")
+                    messages.push("<b>5:</b> D - NAVAID")
                     if (input.length >= 6) { // Проверяем 6 символ
                         switch (input[5]) {
                         case ' ':
-                            messages.push("6 - Blank - VHF NAVAID (D)")
+                            messages.push("<b>6:</b> Blank - VHF NAVAID (D)")
+                            if (input.length >= 10) {
+                                messages.push("<b>" + input.substring(6,10) + ':</b> Airport ICAO Identifier [7-10] (<a href="References/5_6_Airport_Heliport_Identifier.png" target="_blank">5.6</a>)')}
+                            if (input.length >= 12) {
+                                messages.push("<b>" + input.substring(10,12) + ':</b> ICAO Code [11-12] (<a href="References/5_14_ICAO_Code.png" target="_blank">5.14</a>)')}
+                            if (input.length >= 17) {
+                                messages.push("<b>" + input.substring(13,17) + ':</b> VOR Identifier [14-17] (<a href="References/5_33_VOR_NDB_Identifier.png" target="_blank">5.33</a>)')}
+                            if (input.length >= 21) {
+                                messages.push("<b>" + input.substring(19,21) + ':</b> ICAO Code [20-21] (<a href="References/5_14_ICAO_Code.png" target="_blank">5.14</a>)')}
+                            if (input.length >= 27) {
+                                messages.push("<b>" + input.substring(22,27) + ':</b> VOR Frequency [23-27] (<a href="References/5_34_VOR_Frequency.png" target="_blank">5.34</a>)')}
+                            if (input.length >= 32) {
+                                messages.push("<b>" + input.substring(27,32) + ':</b> NAVAID Class [28-32] (<a href="References/5_35_NAVAID_Class.png" target="_blank">5.35</a>)')}
+                            if (input.length >= 41) {
+                                messages.push("<b>" + input.substring(32,41) + ':</b> VOR Latitude [33-41] (<a href="References/5_36_Latitude.png" target="_blank">5.36</a>)')}
+                            if (input.length >= 51) {
+                                messages.push("<b>" + input.substring(41,51) + ':</b> VOR Longitude [42-51] (<a href="References/5_37_Longitude.png" target="_blank">5.37</a>)')}
+                            if (input.length >= 55) {
+                                messages.push("<b>" + input.substring(51,55) + ':</b> DME Identifier [52-55] (<a href="References/5_38_DME_Ident.png" target="_blank">5.38</a>)')}
+                            if (input.length >= 64) {
+                                messages.push("<b>" + input.substring(55,64) + ':</b> DME Latitude [56-64] (<a href="References/5_36_Latitude.png" target="_blank">5.36</a>)')}
+                            if (input.length >= 74) {
+                                messages.push("<b>" + input.substring(64,74) + ':</b> DME Longitude [65-74] (<a href="References/5_37_Longitude.png" target="_blank">5.37</a>)')}
+                            if (input.length >= 79) {
+                                messages.push("<b>" + input.substring(74,79) + ':</b> Station Declination [75-79] (<a href="References/5_66_Station_Declination.png" target="_blank">5.66</a>)')}
+                            if (input.length >= 84) {
+                                messages.push("<b>" + input.substring(79,84) + ':</b> DME Elevation [80-84] (<a href="References/5_40_DME_Elevation.png" target="_blank">5.40</a>)')}
+                            if (input.length >= 85) {
+                                messages.push("<b>" + input[84] + ':</b> Figure of Merit [85] (<a href="References/5_149_Figure_of_Merit.png" target="_blank">5.149</a>)')}
+                            if (input.length >= 87) {
+                                messages.push("<b>" + input.substring(85,87) + ':</b> ILS/DME Bias [86-87] (<a href="References/5_90_ILS_DME_Bias.png" target="_blank">5.90</a>)')}
+                            if (input.length >= 90) {
+                                messages.push("<b>" + input.substring(87,90) + ':</b> Frequency Protection [88-90] (<a href="References/5_150_Frequency_Protection.png" target="_blank">5.150</a>)')}
+                            if (input.length >= 93) {
+                                messages.push("<b>" + input.substring(90,93) + ':</b> Datum Code [91-93] (<a href="References/5_197_Datum_Code.png" target="_blank">5.197</a>)')}
+                            if (input.length >= 123) {
+                                messages.push("<b>" + input.substring(93,123) + ':</b> VOR Name [94-123] (<a href="References/5_71_Name_Field.png" target="_blank">5.71</a>)')}
                             break;
                         case 'B':
                             messages.push("6 - B - NDB NAVAID (DB)")
