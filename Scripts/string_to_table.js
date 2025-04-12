c01d5602f35a0c740f2c8e6388f316374c4d0f7a
Konopleva_IV <inna.konopleva@evraz.com>
2025-04-12 21:04:08 +0300

    скрипт черновик 1

--- Scripts/string_to_table.js
+++ Scripts/string_to_table.js
@@ -1,3 +1,42 @@
+function checkCharacters(input) {
+    let message = '';
+    let messages = [];
+
+    switch (input[0]) {
+        case 'S': // Если первый символ 'S', продолжаем проверку
+            messages.push("1 - S - Standard")
+            if (input.length >= 5) { // Проверяем пятый символ
+                switch (input[4]) {
+                case 'A':
+                    messages.push("5 - A - MORA")
+                    break;
+                case 'D':
+                    messages.push("5 - D - NAVAID")
+                    break;
+                case 'E':
+                    messages.push("5 - E - Enroute")
+                    break;
+                case 'H':
+                    messages.push("5 - H - Heliport")
+                    break;
+                default:
+                    messages.push("5 - ???")
+                    break;
+                }
+            }
+            break;
+        case 'T':
+            messages.push("1 - T - Tailored")
+            break;
+        default:
+            messages.push("1 - ???")
+            break;
+    }
+
+    message = messages.join('<br>'); // Объединяем сообщения с переносом строки
+    return message;
+}
+
 function convertString() {
     const input = document.getElementById('input-string').value;
     if (!input || input.trim().length === 0) return alert("Добавьте строку ARINC в текстовое поле");
@@ -20,9 +59,14 @@ function convertString() {
     // Собираем обе строки вместе и вставляем в таблицу
     const tableContent = `${numberRow}${characterRow}`;
     document.getElementById('result-table').innerHTML = tableContent;
+
+    // Обрабатываем строку для комментариев
+    const message = checkCharacters(input);
+    document.getElementById('message').innerHTML = message;
 }
 
 function clearInput() {
     document.getElementById('input-string').value = ''; // Очищаем поле ввода
     document.getElementById('result-table').innerHTML = ''; // Очищаем таблицу
+    document.getElementById('message').innerHTML = ''; // Очищаем текстовую расшифровку
 }
